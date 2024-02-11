b86a074458b4e04bbd03ca5c8380cabef825dc9b
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2024-02-11 13:39:17 -0600

    Starting with django rest framework

--- Forum/Forum/settings.py
+++ Forum/Forum/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'Noheru.apps.NoheruConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- Forum/Noheru/admin.py
+++ Forum/Noheru/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Users, Posts, Responses
+from .models import User, Post, Response
 
 # Register your models here.
-admin.site.register(Users)
-admin.site.register(Posts)
-admin.site.register(Responses)
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Response)
