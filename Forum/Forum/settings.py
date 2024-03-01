bb1121f43808839cee8bf27c9fe834bf0959f808
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2024-03-01 16:11:25 -0600

    cors-header issue resolved

--- Forum/Forum/settings.py
+++ Forum/Forum/settings.py
@@ -29,8 +29,10 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CORS_ORIGIN_ALLOW_ALL=True
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'Noheru.apps.NoheruConfig',
     'django.contrib.admin',
@@ -42,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
